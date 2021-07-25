7ef1d1ecafdb34fc16551c4dcd24fe31c4815a6a
Shadab <shadab19it@gmail.com>
2021-07-26 01:03:03 +0530

    Updates

---
+++ precache-manifest.6ca2aef95b0295b19cbb4333b4087a8b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee85f62434982085d811b5b12ef189fe",
+    "url": "/watchman-website/index.html"
+  },
+  {
+    "revision": "f1296f3e603e6a5b4e94",
+    "url": "/watchman-website/static/css/main.ca9f791e.chunk.css"
+  },
+  {
+    "revision": "693de5239e0d94ab56a1",
+    "url": "/watchman-website/static/js/2.150c8c45.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/watchman-website/static/js/2.150c8c45.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1296f3e603e6a5b4e94",
+    "url": "/watchman-website/static/js/main.b668db1e.chunk.js"
+  },
+  {
+    "revision": "317b7a2e08c46c173d07",
+    "url": "/watchman-website/static/js/runtime-main.7b61c1b6.js"
+  },
+  {
+    "revision": "f90a8ed40216ffbfdb6dbafea5a78d6e",
+    "url": "/watchman-website/static/media/logo.f90a8ed4.svg"
+  }
+]);
